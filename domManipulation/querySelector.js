15a56b5824380fb07e2a17857e308f6159d67def
SadaySamnotra <sadaysamnotra@gmail.com>
2024-09-30 10:54:45 +0530

    adding changes

---
+++ domManipulation/querySelector.js
@@ -0,0 +1,7 @@
+const elementByClass=document.querySelectorAll('.highlighted');
+const elementByID=document.querySelectorAll('#my-paragraph');
+const elementByTag = document.querySelectorAll('p');
+
+console.log(elementByClass);
+console.log(elementByID);
+console.log(elementByTag);
